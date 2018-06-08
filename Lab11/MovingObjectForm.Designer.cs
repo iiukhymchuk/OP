ffbae88e4d5453f8b5d3133584a026747747633b
Ievgenii Iukhymchuk <ievgenii.iukhymchuk@gmail.com>
2018-06-08 05:58:56 +0300

    Add Lab 11

---
+++ Lab11/MovingObjectForm.Designer.cs
@@ -0,0 +1,74 @@
+﻿namespace Lab11
+{
+    partial class MovingObjectForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.PictureBox = new System.Windows.Forms.PictureBox();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PictureBox
+            // 
+            this.PictureBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.PictureBox.ErrorImage = null;
+            this.PictureBox.InitialImage = null;
+            this.PictureBox.Location = new System.Drawing.Point(12, 12);
+            this.PictureBox.Margin = new System.Windows.Forms.Padding(0);
+            this.PictureBox.Name = "PictureBox";
+            this.PictureBox.Size = new System.Drawing.Size(776, 426);
+            this.PictureBox.TabIndex = 0;
+            this.PictureBox.TabStop = false;
+            this.PictureBox.Paint += new System.Windows.Forms.PaintEventHandler(this.PictureBox_Paint);
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Tick += new System.EventHandler(this.timer_Tick);
+            // 
+            // MovingObjectForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.PictureBox);
+            this.Name = "MovingObjectForm";
+            this.Text = "Moving object";
+            ((System.ComponentModel.ISupportInitialize)(this.PictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox PictureBox;
+        private System.Windows.Forms.Timer timer;
+    }
+}
+
---
+++ Lab11/MovingObjectForm.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+namespace Lab11
+{
+    public partial class MovingObjectForm : Form
+    {
+        int x;
+        int y;
+        int xDirection;
+        int yDirection;
+        int pictureHeight;
+        int pictureWidth;
+
+        public MovingObjectForm()
+        {
+            InitializeComponent();
+            var random = new Random();
+            x = 300;
+            y = 100;
+            xDirection = (int)(random.NextDouble() * 10) * ((random.Next() % 2) == 0 ? -1 : 1);
+            yDirection = (int)(random.NextDouble() * 10) * ((random.Next() % 2) == 0 ? -1 : 1);
+            pictureHeight = 200;
+            pictureWidth = 200;
+        }
+
+        private void PictureBox_Paint(object sender, PaintEventArgs e)
+        {
+            Point startPoint = new Point(x, y);
+            e.Graphics.DrawImage(new Bitmap(Properties.Resources.Racoon), startPoint);
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            var borderWidth = PictureBox.Size.Width;
+            var borderHeight = PictureBox.Size.Height;
+
+            if (x <= 0 || x >= borderWidth - pictureWidth)
+            {
+                xDirection *= -1;
+            }
+            if (y <= 0 || y >= borderHeight - pictureHeight)
+            {
+                yDirection *= -1;
+            }
+
+            x += xDirection;
+            y += yDirection;
+
+            PictureBox.Invalidate();
+        }
+    }
+}
